601ea14e11a3b2dc5e82d22e98b41b836e8b0622
Jeremy <jeremywhitenect@gmail.com>
2025-01-17 20:49:42 -0400

    Does major refactoring

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-document.body.addEventListener("click", (ev) => {
-    const isExpandableTitle = !!ev.target.closest(".expandable-icon");
-    const expandable = ev.target.closest(".expandable");
+Promise.all([
+    fetch('components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        }),
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        }),
+    fetch('components/icon-bars.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("icon-bars-container").innerHTML = data;
+        }),
+])
+.catch(error => console.error('Error loading components:', error))
+.finally(addEventListeners());
 
-    if (!isExpandableTitle) {
-        return;
-    }
-    expandable.classList.toggle("expandable-open");
-});
+function addEventListeners() {
+    document.body.addEventListener("click", (ev) => {
+        const isExpandableTitle = !!ev.target.closest(".expandable-icon");
+        const expandable = ev.target.closest(".expandable");
+    
+        if (!isExpandableTitle) {
+            return;
+        }
+        expandable.classList.toggle("expandable-open");
+    });
+    
+}
